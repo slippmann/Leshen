66d80113c483ab76fa9d91f20a872c6bbf8054ab
slippmann <steven.lippmann1@gmail.com>
2021-01-15 21:08:46 -0800

    #8 - Added documentation to the ILogService.

--- ILogService.h
+++ ILogService.h
@@ -2,6 +2,10 @@
 
 #include "IService.h"
 
+////////////////////
+/// \brief Logging service interface.
+///
+////////////////////
 class ILogService : public IService
 {
 public:
@@ -13,9 +17,51 @@ public:
 		ERR
 	};
 
+	////////////////////
+	/// \brief Set the log level.
+	/// 
+	/// \param LogLevel		The lowest level to log.
+	/// 
+	////////////////////
 	virtual void SetLevel(LogLevel level) = 0;
+
+	////////////////////
+	/// \brief Log a message at info level.
+	/// Formatting same as printf.
+	/// 
+	/// \param formatString	string to log.
+	/// \param ...		additional args dependent on formatString.
+	/// 
+	////////////////////
 	virtual void Info(const char* formatString, ...) = 0;
+
+	////////////////////
+	/// \brief Log a message at debug level.
+	/// Formatting same as printf.
+	/// 
+	/// \param formatString	string to log.
+	/// \param ...		additional args dependent on formatString.
+	/// 
+	////////////////////
 	virtual void Debug(const char* formatString, ...) = 0;
+
+	////////////////////
+	/// \brief Log a message at warning level.
+	/// Formatting same as printf.
+	/// 
+	/// \param formatString	string to log.
+	/// \param ...		additional args dependent on formatString.
+	/// 
+	////////////////////
 	virtual void Warn(const char* formatString, ...) = 0;
+	
+	////////////////////
+	/// \brief Log a message at error level.
+	/// Formatting same as printf.
+	/// 
+	/// \param formatString	string to log.
+	/// \param ...		additional args dependent on formatString.
+	/// 
+	////////////////////
 	virtual void Error(const char* formatString, ...) = 0;
 };
